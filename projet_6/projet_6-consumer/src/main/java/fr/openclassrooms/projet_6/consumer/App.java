6b53404473dd306dac103df764cb2809189f0bd4
Ayrton <asadams89@gmail.com>
2019-04-22 17:25:33 +0200

    Création du projet Maven, ajout des differents modules et de leurs dependances dans les differents POMs

---
+++ projet_6/projet_6-consumer/src/main/java/fr/openclassrooms/projet_6/consumer/App.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.projet_6.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ projet_6/projet_6-model/src/main/java/fr/openclassrooms/projet_6/model/App.java
@@ -0,0 +1,13 @@
+package fr.openclassrooms.projet_6.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
